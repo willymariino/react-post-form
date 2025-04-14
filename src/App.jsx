e8d198495c5bf3581455324d3d8ca06d2da67238
willymariino <wg.mariino@gmail.com>
2025-04-14 16:13:05 +0200

    added formPost

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,20 @@ import { useState } from "react"
 
 function App() {
 
+  const [posts, setPosts] = useState([])
+
+  const [formPost, setFormPost] = useState({
+    author: "author",
+    title: "title",
+    body: "testo del post",
+    public: false
+
+  })
+
+  function handlePost(event) {
+
+
+  }
 
   return (
     <>
