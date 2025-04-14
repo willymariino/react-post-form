bec0bed0e63a9898dc6d7264127479731d92f327
willymariino <wg.mariino@gmail.com>
2025-04-14 17:13:14 +0200

    added setFormposts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useState } from "react"
+import { formatPostcssSourceMap } from "vite";
 
 function App() {
 
-  const [posts, setPosts] = useState([])
 
-  const [formPost, setFormPost] = useState({
+
+  const [formPosts, setFormPosts] = useState({
     author: "author",
     title: "title",
     body: "testo del post",
@@ -13,11 +14,37 @@ function App() {
 
   })
 
-  function handlePost(event) {
+  function handleFormData(event) {
+
+    // 1) prendere il valore corretto in base al tipo
+    let value;
+    if (event.target.value.type === "checkbox") {
+      value = event.taget.checked // se il campo è una checkbox, react non usa value, ma checkbox.}
+    }
+
+    else {
+      value.target.value
+    }
+
+
+    // 2) se è un numero, convertilo da stringa a numero
+    if (event.target.type === "number") {
+      value = parseInt(value)
+    }
+
+    // 3) aggiorna solo il campo modifiche nello stato
+
+    setFormPosts(formPosts => ({
+      ...formPosts,
+      [event.target.name]: value
+
+
+    })
+
+    )
 
 
   }
-
   return (
     <>
 
